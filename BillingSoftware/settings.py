cd44ca0ddcb8700b724eeeeadc691a7165a65718
Muhammed <rahilmuhammed0435@gmail.com>
2025-02-14 01:39:56 +0530

    Switched

--- BillingSoftware/settings.py
+++ BillingSoftware/settings.py
@@ -68,8 +68,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('MYSQL_DATABASE', 'railway'),
         'USER': os.getenv('MYSQL_USER', 'root'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'pVIaNlrIRGOiYeUUhGsltezCsVxAtxYw'),
-        'HOST': os.getenv('MYSQL_HOST', 'localhost'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', ''),
+        'HOST': os.getenv('MYSQL_HOST', 'mysql.railway.internal'),
         'PORT': os.getenv('MYSQL_PORT', '3306'),
         'OPTIONS': {
             'charset': 'utf8mb4',
