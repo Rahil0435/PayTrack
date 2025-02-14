88e99a7b6706ac741da77b290398fb632c80103e
Muhammed <rahilmuhammed0435@gmail.com>
2025-02-14 16:01:15 +0530

    Switched1

--- BillingSoftware/settings.py
+++ BillingSoftware/settings.py
@@ -63,11 +63,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'BillingSoftware.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
-if not DATABASES['default']:
-    raise ValueError("DATABASE_URL environment variable is not set.")
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
