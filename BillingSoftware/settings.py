946aabb9fa4874ddbc185cfa9d21c9298a347138
Muhammed <rahilmuhammed0435@gmail.com>
2025-02-14 15:05:04 +0530

    Switched1

--- BillingSoftware/settings.py
+++ BillingSoftware/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["yourdomain.com", "paytrack.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [ "https://paytrack-tosp.onrender.com", "localhost", "127.0.0.1"]
 
 
 
