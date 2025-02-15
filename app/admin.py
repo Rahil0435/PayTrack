e317539ffa5ac36fc3b3b95b77a9c45a35c2f966
Muhammed <rahilmuhammed0435@gmail.com>
2025-02-16 00:20:30 +0530

    hiy

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Invoice,reg,ProductionHistory,Product,InvoiceItem,login,Production
 
 # Register your models here.
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity') 
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'date', 'total_amount') 
+@admin.register(reg)  
+class regAdmin(admin.ModelAdmin):
+    list_display = ('name',  'phno')
+@admin.register(login)
+class loginAdmin(admin.ModelAdmin):
+    list_display = ('uname', 'pswd')
+
+@admin.register(Production)
+class ProductionAdmin(admin.ModelAdmin):
+    list_display = ( 'product', 'quantity_added')
